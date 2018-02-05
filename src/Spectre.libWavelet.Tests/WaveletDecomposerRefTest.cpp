55ce5c7ddbfaabf666eec935dfe541d5216d48f5
Michal 'Sand3r' Gallus <sand3r@interia.eu>
2018-02-05 19:06:32 +0100

    Add reference implementation of wavelet decomposer

---
+++ src/Spectre.libWavelet.Tests/WaveletDecomposerRefTest.cpp
@@ -0,0 +1,171 @@
+/*
+* WaveletDecomposerRefTest.cpp
+* Tests whether the signal is properly decomposed by the reference 
+* implementation.
+*
+Copyright 2018 Michal Gallus
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <gtest/gtest.h>
+#include "Spectre.libWavelet\WaveletDecomposerRef.h"
+
+namespace
+{
+    using namespace spectre::algorithm::wavelet;
+
+    static void CompareVectors(const std::vector<DataType>& estimate, const std::vector<DataType>& correct)
+    {
+        constexpr DataType maxAbsoluteError = 0.0001;
+        for (unsigned i = 0; i < estimate.size(); i++)
+        {
+            ASSERT_NEAR(estimate[i], correct[i], maxAbsoluteError);
+        }
+    }
+
+    TEST(WaveletDecomposerInitialization, initializes)
+    {
+        EXPECT_NO_THROW(WaveletDecomposerRef());
+    }
+
+    class WaveletDecomposerRefTest : public ::testing::Test
+    {
+    public:
+        WaveletDecomposerRefTest()
+        {
+            lastRowLastHighFrequencyResult = {
+                -1.00930961719589e-05,
+                0.000631661429085606,
+                0.00363953257484134,
+                -0.0349096779749494,
+                0.0939060392824094,
+                -0.573668491474043,
+                1.48135002884348,
+                -1.25125371723593,
+                -0.0680747992002441,
+                0.371157080651903,
+                0.0638308206668811,
+                -0.0652726295392059,
+                -0.0213226552393410,
+                -3.09968872155731e-06,
+                0.0
+            };
+            firstRowLastHighFrequencyResult = {
+                0.0,
+                -8.84442489411620e-06,
+                0.000586138712079808,
+                0.00366319641253592,
+                -0.0337088162678507,
+                0.0882423835249452,
+                -0.562277648805622,
+                1.47065650410807,
+                -1.24839451703031,
+                -0.0660665610544188,
+                0.370413811253544,
+                0.0633207123092398,
+                -0.0651742255517986,
+                -0.0212504983010336,
+                -1.63488447864066e-06
+            };
+            firstHighFrequencyResult = {
+                0,
+                -0.230377813308897,
+                0.254090943935123,
+                0.107678933249283,
+                -0.0667168468534168,
+                -0.0540778152370236,
+                -0.0105974017850692,
+                -2.22044604925031e-16,
+                -2.22044604925031e-16,
+                -8.88178419700125e-16,
+                2.30377813308897,
+                -4.61430975913130,
+                1.21002916292327,
+                1.63627886777771,
+                -0.0596734693105170,
+                -0.380726319282519,
+                -0.0953766160656215
+            };
+            firstLowFrequencyResult = {
+                0.0,
+                -4.06844404044861e-07,
+                2.69624376778159e-05,
+                0.000170805548639498,
+                -0.00170291259939498,
+                0.00309960587361391,
+                -0.0165956761940691,
+                0.0479600502210081,
+                0.0561611188212727,
+                -0.339939690673109,
+                -0.0487037936781232,
+                1.23299325679310,
+                1.41702876032074,
+                0.461966379081157,
+                3.55408918941385e-05,
+                0.0
+            };
+            lastLowFrequencyResult = {
+                -4.64283404088808e-07,
+                2.90564870809657e-05,
+                0.000170041467736160,
+                -0.00176998109340235,
+                0.00336519663747599,
+                -0.0167679988715969,
+                0.0469668204068491,
+                0.0577080776967957,
+                -0.338551325051386,
+                -0.0523643871799257,
+                1.23076472595957,
+                1.41934784856588,
+                0.463535004863161,
+                6.73843951653069e-05,
+                0.0,
+                0.0
+            };
+        }
+
+        void InitializeSignal(Signal& signal)
+        {
+            for (unsigned i = 0; i < signal.size(); i++)
+            {
+                signal[i] = (DataType)i;
+            }
+        }
+
+    protected:
+        WaveletDecomposerRef decomposer;
+        std::vector<DataType> lastRowLastHighFrequencyResult;
+        std::vector<DataType> firstRowLastHighFrequencyResult;
+        std::vector<DataType> firstHighFrequencyResult;
+        std::vector<DataType> firstLowFrequencyResult;
+        std::vector<DataType> lastLowFrequencyResult;
+    };
+
+    TEST_F(WaveletDecomposerRefTest, decomposes_the_signal)
+    {
+        Signal signal(10);
+        InitializeSignal(signal);
+        WaveletCoefficients coefficients = decomposer.Decompose(std::move(signal));
+
+        // Due to high amount of coefficients generated, only a few sets will be
+        // compared
+        CompareVectors(coefficients.data[0][0], firstHighFrequencyResult);
+        CompareVectors(coefficients.data[WAVELET_LEVELS - 1][0], firstRowLastHighFrequencyResult);
+        CompareVectors(coefficients.data[WAVELET_LEVELS - 1][(1 << (WAVELET_LEVELS - 1)) - 1],
+            lastRowLastHighFrequencyResult);
+        CompareVectors(coefficients.data[WAVELET_LEVELS][0], firstLowFrequencyResult);
+        CompareVectors(coefficients.data[WAVELET_LEVELS][(1 << (WAVELET_LEVELS - 1)) - 1],
+            lastLowFrequencyResult);
+    }
+}
---
+++ src/Spectre.libWavelet/WaveletDecomposerRef.cpp
@@ -0,0 +1,126 @@
+/*
+* WaveletDecomposerRef.cpp
+* Reference implementation of wavelet decomposer.
+*
+* Bibliography:
+* https://en.wikipedia.org/wiki/Stationary_wavelet_transform#Implementation
+* http://web.iitd.ac.in/~sumeet/WaveletTutorial.pdf
+* http://www.egr.msu.edu/~aviyente/ece802swt.ppt
+*
+Copyright 2018 Michal Gallus
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "WaveletDecomposerRef.h"
+#include "PrecomputedDaubechiesCoefficients.h"
+
+namespace spectre::algorithm::wavelet
+{
+static constexpr size_t ComputeScale(unsigned level)
+{
+    return 1ULL << (level + 1); // 2^(L + 1)
+}
+
+static constexpr size_t CeiledDivision(size_t dividend, size_t divisor)
+{
+    return dividend / divisor + (dividend % divisor != 0);
+}
+
+static inline size_t ComputeBlockLength(unsigned level, size_t scale, size_t signalLength)
+{
+    constexpr unsigned BASIS_LENGTH = (WAVELET_BASIS * 2) - 1;
+
+    const size_t numerator = signalLength + ((1 << level) - 1) * BASIS_LENGTH;
+    const size_t denominator = scale;
+    const size_t blockLength = CeiledDivision(numerator, denominator) + BASIS_LENGTH;
+
+    return blockLength;
+}
+
+// The function performs an 'intelligent' downsampling, where every second coefficient is
+// stored as a coefficient of 'new' sample, instead of just being discarded. This allows
+// the decomposition to retain more information about the signal.
+static inline void DownsampleByDissolving(CoefficientsPerLevel& coefficients, unsigned level, unsigned blockLength)
+{
+    const unsigned shift = (1 << level) >> 1;
+    for (unsigned i = 0; i < shift; i++)
+    {
+        coefficients[shift + i].resize(blockLength);
+        unsigned oldRowColumn = 0;
+        unsigned newRowColumn = 0;
+        for (unsigned j = 0; j < coefficients[i].size(); j++)
+        {
+            unsigned row = j % 2 ? shift + i : i;
+            unsigned column = j % 2 ? newRowColumn++ : oldRowColumn++;
+            coefficients[row][column] = coefficients[i][j];
+        }
+        coefficients[i].resize(oldRowColumn); // Used to fill moved entries with zeros.
+        coefficients[i].resize(blockLength);
+    }
+}
+
+// Applies the high and low pass filters on the current low-frequency
+// wavelet coefficients, at the level determined by the caller.
+// https://upload.wikimedia.org/wikipedia/commons/1/16/Wavelets_-_SWT_Filter_Bank.png
+// The figure above depicts the process, where h_j prepresents a high- and g_j a low-pass
+// filters, applied at level j.
+// All the low frequency coefficients are overridden on each call, while the high
+// frequnecy ones are just added.
+inline void WaveletDecomposerRef::ApplyFilters(WaveletCoefficients& coefficients,
+    CoefficientsPerLevel& lowFrequencyCoefficients, size_t scale, size_t blockLength, unsigned level) const
+{
+    coefficients.data[level].resize(scale); // Adjust number of coefficient lists for that certain level
+    for (unsigned i = 0; i < scale; i++)
+    {
+        lowFrequencyCoefficients[i].resize(blockLength); // Extend with zeros/shrink to match size
+        coefficients.data[level][i] = // Apply Daubechies4 decomposition high pass filter
+            m_Convolution.Convolve(precomputed::DecompositionHighPassFilter, lowFrequencyCoefficients[i]);
+        lowFrequencyCoefficients[i] = // Apply Daubechies4 decomposition low pass filter
+            m_Convolution.Convolve(precomputed::DecompositionLowPassFilter, lowFrequencyCoefficients[i]);
+    }
+}
+
+WaveletDecomposerRef::WaveletDecomposerRef() : m_Convolution()
+{
+};
+
+WaveletCoefficients WaveletDecomposerRef::Decompose(Signal&& signal) const
+{
+    const size_t signalLength = signal.size();
+
+    WaveletCoefficients coefficients;
+    CoefficientsPerLevel& lowFrequencyCoefficients = coefficients.data[WAVELET_LEVELS];
+
+    // The number of samples N used to generate N coefficient lists
+    // for the lowest frequency decomposition.
+    constexpr size_t LOW_FREQ_SAMPLES_NUM = 1 << (WAVELET_LEVELS - 1); // 2^(WL - 1)
+    lowFrequencyCoefficients.resize(LOW_FREQ_SAMPLES_NUM);
+    lowFrequencyCoefficients[0] = std::move(signal); // Initialize first line with signal
+
+    size_t scale = 1; // Initial wavelet scale (or it's frequency)
+    size_t blockLength = ComputeBlockLength(0, scale, signalLength); // Initial number of coefficients
+
+    for (unsigned currentLevel = 0; currentLevel < WAVELET_LEVELS - 1; currentLevel++)
+    {
+        ApplyFilters(coefficients, lowFrequencyCoefficients, scale, blockLength, currentLevel);
+
+        scale = ComputeScale(currentLevel);
+        blockLength = ComputeBlockLength(currentLevel + 1, scale, signalLength);
+        DownsampleByDissolving(lowFrequencyCoefficients, currentLevel + 1, (unsigned)blockLength);
+    }
+
+    ApplyFilters(coefficients, lowFrequencyCoefficients, scale, blockLength, WAVELET_LEVELS - 1);
+
+    return coefficients;
+}
+}
