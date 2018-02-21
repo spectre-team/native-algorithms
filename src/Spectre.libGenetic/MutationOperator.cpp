e61be7f73deb70ea038fee99ca9cae883a2ccbf7
Regis128 <regis1286@gmail.com>
2018-02-21 17:27:41 +0100

    cosmetic changes for Michal review

--- src/Spectre.libGenetic/MutationOperator.cpp
+++ src/Spectre.libGenetic/MutationOperator.cpp
@@ -29,13 +29,11 @@ MutationOperator::MutationOperator(double mutationRate, double bitSwapRate, Seed
     m_RandomNumberGenerator(rngSeed),
     m_IndividualFeasibilityCondition(condition)
 {
-    if (m_MutationRate >= 0 && m_MutationRate <= 1) {}
-    else
+    if (m_MutationRate < 0 || m_MutationRate > 1)
     {
         throw spectre::core::exception::ArgumentOutOfRangeException<double>("mutationRate", 0, 1, m_MutationRate);
     }
-    if (m_BitSwapRate >= 0 && m_BitSwapRate <= 1) {}
-    else
+    if (m_BitSwapRate < 0 || m_BitSwapRate > 1)
     {
         throw spectre::core::exception::ArgumentOutOfRangeException<double>("bitSwapRate", 0, 1, m_BitSwapRate);
     }
