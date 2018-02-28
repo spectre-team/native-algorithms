b93e953d1838ce514deb23f56c45393c2c851538
Regis128 <regis1286@gmail.com>
2018-02-28 22:05:42 +0100

    new function in OpenCvDataset to shorten the code

--- src/Spectre.libClassifier/OpenCvDataset.cpp
+++ src/Spectre.libClassifier/OpenCvDataset.cpp
@@ -21,6 +21,7 @@ limitations under the License.
 #include "Spectre.libException/OutOfRangeException.h"
 #include "OpenCvDataset.h"
 #include "Spectre.libClassifier/EmptyOpenCvDatasetException.h"
+#include "Spectre.libFunctional/Filter.h"
 
 namespace spectre::supervised {
 
@@ -121,7 +122,22 @@ gsl::span<const Label> OpenCvDataset::GetSampleMetadata() const
     return m_labels;
 }
 
-size_t OpenCvDataset::size() const
+OpenCvDataset OpenCvDataset::getFilteredOpenCvDataset(std::vector<bool> filterPattern) const
+{
+    const auto& dataToFilter = this->GetData();
+    const auto twoDimensionalFilteredData = core::functional::filter(dataToFilter, filterPattern);
+    std::vector<DataType> oneDimensionalFilteredData;
+    oneDimensionalFilteredData.reserve(twoDimensionalFilteredData.size() * twoDimensionalFilteredData[0].size());
+    for (const auto& observation : twoDimensionalFilteredData)
+    {
+        oneDimensionalFilteredData.insert(oneDimensionalFilteredData.end(), observation.begin(), observation.end());
+    }
+    const auto filteredLabels = core::functional::filter(this->GetSampleMetadata(), filterPattern);
+    OpenCvDataset individualDataset(oneDimensionalFilteredData, filteredLabels);
+    return individualDataset;
+}
+
+    size_t OpenCvDataset::size() const
 {
     return m_labels.size();
 }
