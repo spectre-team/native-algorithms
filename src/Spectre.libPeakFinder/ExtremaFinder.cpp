060581f6bd823d28dd886e8e2c4a199c835f6b88
Michal 'Sand3r' Gallus <sand3r@interia.eu>
2018-03-04 20:03:37 +0100

    Implement ExtremaFinder

---
+++ src/Spectre.libPeakFinder/ExtremaFinder.cpp
@@ -0,0 +1,100 @@
+/*
+ * ExtremaFinder.cpp
+ * Finding indices of extrema in signals.
+ *
+ Copyright 2018 Spectre Team
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include "ExtremaFinder.h"
+#include "Spectre.libStatistics\Math.h"
+#include "Spectre.libFunctional\Filter.h"
+
+namespace spectre::algorithm::peakfinder
+{
+using namespace spectre::statistics::basic_math;
+using namespace spectre::core::functional;
+
+// Branchless cache-efficient zero filter
+static inline Indices GetNonZeros(const Signal& data)
+{
+    const size_t length = data.size();
+    std::vector<unsigned> indices(length + 1);
+    unsigned zeros = 0;
+
+    for (unsigned i = 0; i < length; i++)
+    {
+        bool dataIsZero = data[i] == 0.0;
+        int index = i - zeros + dataIsZero;
+        zeros += dataIsZero;
+
+        indices[index] = i;
+    }
+    indices.resize(length - zeros);
+
+    return indices;
+}
+
+// This function finds indices of intensities that combined together create signal with removed flat lines.
+static inline Indices FilterFlatSegmentIndices(const SignalView& signal)
+{
+    Signal derivatives = differentiate(signal);
+    Indices indicesOfNonZeros = GetNonZeros(derivatives);
+    indicesOfNonZeros.push_back((unsigned)(signal.size() - 1));
+    return indicesOfNonZeros;
+}
+
+static inline Signal DifferentiateFilteredSignal(const SignalView& signal, const Indices& indices)
+{
+    Signal filteredSignal = filter<DataType, Index>(signal, gsl::as_span(indices));
+    return differentiate_unsafe(gsl::as_span(filteredSignal)); // Unsafe is used here, because the exception
+}                                                              // would've already been thrown in FilterFlatSegmentIndices
+
+// Comparison operations are interchangably std::greater_equal and std::less_equal
+// skipFirstAndLast is added for the purpose of peak finding, when the peak is
+// assumed to never start or end of the signal.
+using ComparisonOp = std::function<bool(DataType, DataType)>;
+static Indices FindExtrema(const SignalView& signal, ComparisonOp leftComparison,
+    ComparisonOp rightComparison, bool skipFirstAndLast = false)
+{
+    Indices filteredIndices = FilterFlatSegmentIndices(signal);
+    Signal derivatives = DifferentiateFilteredSignal(signal, filteredIndices);
+
+    Indices extrema;
+    // special case for 0th element
+    if (!skipFirstAndLast && rightComparison(derivatives[0], 0.0))
+        extrema.push_back(filteredIndices[0]);
+
+    // regular processsing for finding extrema
+    for (unsigned i = 1; i < derivatives.size(); i++)
+    {
+        if (leftComparison(derivatives[i - 1], 0.0) && rightComparison(derivatives[i], 0.0))
+            extrema.push_back(filteredIndices[i]);
+    }
+
+    // special case for nth element
+    if (!skipFirstAndLast && leftComparison(*(derivatives.end() - 1), 0.0))
+        extrema.push_back(filteredIndices[derivatives.size()]);
+
+    return extrema;
+}
+
+Indices ExtremaFinder::FindValleys(const SignalView & signal) const
+{
+    return FindExtrema(signal, std::less_equal<DataType>(), std::greater_equal<DataType>());
+}
+Indices ExtremaFinder::FindPeaks(const SignalView & signal) const
+{
+    return FindExtrema(signal, std::greater_equal<DataType>(), std::less_equal<DataType>(), true);
+}
+}
---
+++ src/Spectre.libPeakFinder/ExtremaFinder.h
@@ -0,0 +1,45 @@
+/*
+ * ExtremaFinder.h
+ * Finding indices of extrema in signals.
+ *
+ Copyright 2018 Spectre Team
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+#pragma once
+#include "DataTypes.h"
+#include <vector>
+
+namespace spectre::algorithm::peakfinder
+{
+/// <summary>
+/// Finds extrema in the signal.
+/// </summary>
+class ExtremaFinder
+{
+public:
+    /// <summary>
+    /// Finds indices of minima of the signal.
+    /// </summary>
+    /// <param name="signal">Signal in which minima shall be found.</param>
+    /// <returns>Indices of minima in the signal.</returns>
+    Indices FindValleys(const SignalView& signal) const;
+    /// <summary>
+    /// Finds indices of maxima of the signal.
+    /// </summary>
+    /// <param name="signal">Signal in which maxima shall be found.</param>
+    /// <returns>Indices of maxima in the signal.</returns>
+    Indices FindPeaks(const SignalView& signal) const;
+};
+}
