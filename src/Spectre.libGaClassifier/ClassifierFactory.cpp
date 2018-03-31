65b75bd81581c4c63513a7b67c5b6fcf10f1baa6
Regis128 <regis1286@gmail.com>
2018-03-31 17:38:46 +0200

    create classifierFactory for pybind

---
+++ src/Spectre.libGaClassifier/ClassifierFactory.cpp
@@ -0,0 +1,61 @@
+/*
+* ClassifierFactory.cpp
+* The factory for creating classifiers
+*
+Copyright 2018 Spectre Team
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "ClassifierFactory.h"
+#include "Spectre.libClassifier/Svm.h"
+
+namespace spectre::supervised
+{
+
+ClassifierFactory::ClassifierFactory() {}
+
+Svm ClassifierFactory::buildSvm(unsigned iterationsLimit, double tolerance) const
+{
+    Svm svm(iterationsLimit, tolerance);
+    return svm;
+}
+
+GaClassifier ClassifierFactory::buildGaClassifier(
+    std::string name,
+    double trainingSetSplitRate,
+    double mutationRate,
+    double bitSwapRate,
+    double preservationRate,
+    unsigned generationsNumber,
+    unsigned populationSize,
+    unsigned initialFillup,
+    spectre::algorithm::genetic::Seed seed,
+    size_t minimalFillup,
+    size_t maximalFillup,
+    unsigned int iterationsLimit,
+    double tolerance) const
+{
+    std::unique_ptr<IClassifier> classifier;
+    if (name == "svm")
+    {
+        Svm svm(iterationsLimit, tolerance);
+        classifier = std::make_unique<Svm>(svm);
+    }
+    GaClassifier gaClassifier(std::move(classifier), trainingSetSplitRate, mutationRate, bitSwapRate, preservationRate, generationsNumber,
+        populationSize, initialFillup, seed, minimalFillup, maximalFillup);
+    return gaClassifier;
+}
+
+
+}
---
+++ src/Spectre.libGaClassifier/ClassifierFactory.h
@@ -0,0 +1,58 @@
+/*
+* ClassifierFactory.h
+* The factory for creating classifiers
+*
+Copyright 2018 Spectre Team
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+#include "Spectre.libClassifier/Svm.h"
+#include "Spectre.libGenetic/DataTypes.h"
+#include "GaClassifier.h"
+
+namespace spectre::supervised
+{
+/// <summary>
+/// The factory for creating classifiers
+/// </summary>
+class ClassifierFactory
+{
+public:
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClassifierFactory"/> class.
+    /// </summary>
+    ClassifierFactory();
+    /// <summary>
+    /// Gets svm classifier
+    /// </summary>
+    Svm buildSvm(unsigned int iterationsLimit = 100, double tolerance = 1e-6) const;
+    /// <summary>
+    /// Gets GaClassifier
+    /// </summary>
+    GaClassifier buildGaClassifier(std::string name,
+        double trainingSetSplitRate,
+        double mutationRate,
+        double bitSwapRate,
+        double preservationRate,
+        unsigned int generationsNumber,
+        unsigned int populationSize,
+        unsigned int initialFillup,
+        spectre::algorithm::genetic::Seed seed = 0,
+        size_t minimalFillup = 1ul,
+        size_t maximalFillup = std::numeric_limits<size_t>::max(),
+        unsigned int iterationsLimit = 100,
+        double tolerance = 1e-6) const;
+};
+}
