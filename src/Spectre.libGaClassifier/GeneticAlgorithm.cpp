ef9f1e156991b5f4eac761707e50f47b4196076f
Regis128 <regis1286@gmail.com>
2018-04-11 18:42:46 +0200

    added pybind and opencv dependencies to libGaClassifier

---
+++ src/Spectre.libGaClassifier/GeneticAlgorithm.cpp
@@ -0,0 +1,24 @@
+#include <pybind11/pybind11.h>
+#include "Spectre.libClassifier/IClassifier.h"
+#include "Spectre.libClassifier/DatasetFactory.h"
+#include "ClassifierFactory.h"
+
+namespace py = pybind11;
+
+namespace spectre::supervised {
+PYBIND11_MODULE(genetic_algorithm, m) {
+    py::class_<IClassifier> iClassifier(m, "IClassifier");
+    iClassifier.def("Fit", &IClassifier::Fit, "Fit the data")
+        .def("Predict", &IClassifier::Predict, "Predict the data")
+        .def("clone", &IClassifier::clone, "Clone the classifier");
+
+    py::class_<DatasetFactory> datasetFactory(m, "DatasetFactory");
+    datasetFactory.def(py::init<>())
+        .def("create", &DatasetFactory::create, "create openCvDataset");
+
+    py::class_<ClassifierFactory> gaClassifier(m, "ClassifierFactory");
+    gaClassifier.def(py::init<>())
+        .def("buildSvm", &ClassifierFactory::buildSvm)
+        .def("buildGaClassifier", &ClassifierFactory::buildGaClassifier);
+}
+}
