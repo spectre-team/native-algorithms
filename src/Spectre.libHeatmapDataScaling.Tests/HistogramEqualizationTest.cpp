5549e7020da86e064106377133abeaaf4a6ec412
danielbabiak <daniel.babiak8@gmail.com>
2018-04-29 10:31:32 +0200

    Fix whitespaces

--- src/Spectre.libHeatmapDataScaling.Tests/HistogramEqualizationTest.cpp
+++ src/Spectre.libHeatmapDataScaling.Tests/HistogramEqualizationTest.cpp
@@ -38,55 +38,55 @@ namespace
         }
     };
 
-	TEST_F(HistogramEqualizationTest, throw_when_data_out_of_max_range)
-	{
+    TEST_F(HistogramEqualizationTest, throw_when_data_out_of_max_range)
+    {
         std::vector<double> outOfRangeIntensities = { 0.0, 3.0, 5.0, 260 };
         EXPECT_THROW(histogramEqualization.scaleData(outOfRangeIntensities), ArgumentOutOfRangeException<double>);
-	}
+    }
 
-	TEST_F(HistogramEqualizationTest, throw_when_data_out_of_min_range)
-	{
-		std::vector<double> outOfRangeIntensities = { -3.0, 3.0, 5.0, 260 };
-		EXPECT_THROW(histogramEqualization.scaleData(outOfRangeIntensities), ArgumentOutOfRangeException<double>);
-	}
+    TEST_F(HistogramEqualizationTest, throw_when_data_out_of_min_range)
+    {
+        std::vector<double> outOfRangeIntensities = { -3.0, 3.0, 5.0, 260 };
+        EXPECT_THROW(histogramEqualization.scaleData(outOfRangeIntensities), ArgumentOutOfRangeException<double>);
+    }
 
-	TEST_F(HistogramEqualizationTest, returns_counted_repeating_values)
-	{
+    TEST_F(HistogramEqualizationTest, returns_counted_repeating_values)
+    {
         std::vector<uint8_t> vectorContainingRepeatingValues = { 1, 2, 1, 5, 2, 1 };
-		auto result = histogramEqualization.countRepeatingValues(vectorContainingRepeatingValues);
-		EXPECT_EQ(result[1], 3);
-		EXPECT_EQ(result[2], 2);
-		EXPECT_EQ(result[3], 0);
-		EXPECT_EQ(result[4], 0);
-		EXPECT_EQ(result[5], 1);
-	}
+        auto result = histogramEqualization.countRepeatingValues(vectorContainingRepeatingValues);
+        EXPECT_EQ(result[1], 3);
+        EXPECT_EQ(result[2], 2);
+        EXPECT_EQ(result[3], 0);
+        EXPECT_EQ(result[4], 0);
+        EXPECT_EQ(result[5], 1);
+    }
 
-	TEST_F(HistogramEqualizationTest, returns_correct_size_of_repeating_values)
-	{
-		std::vector<uint8_t> vectorContainingRepeatingValues = { 1, 2, 1, 5, 2, 1 };
-		auto result = histogramEqualization.countRepeatingValues(vectorContainingRepeatingValues);
-		EXPECT_EQ(result.size(), 256);
-	}
+    TEST_F(HistogramEqualizationTest, returns_correct_size_of_repeating_values)
+    {
+        std::vector<uint8_t> vectorContainingRepeatingValues = { 1, 2, 1, 5, 2, 1 };
+        auto result = histogramEqualization.countRepeatingValues(vectorContainingRepeatingValues);
+        EXPECT_EQ(result.size(), 256);
+    }
 
-	TEST_F(HistogramEqualizationTest, returns_scaled_intensities)
-	{
-		auto result = histogramEqualization.scaleData(testIntensities);
-		EXPECT_THAT(result, testing::ContainerEq(std::vector<double>{ 142.0, 142.0, 57.0, 57.0, 255.0, 198.0, 198.0, 0.0, 85.0, 255.0 }));
-	}
+    TEST_F(HistogramEqualizationTest, returns_scaled_intensities)
+    {
+        auto result = histogramEqualization.scaleData(testIntensities);
+        EXPECT_THAT(result, testing::ContainerEq(std::vector<double>{ 142.0, 142.0, 57.0, 57.0, 255.0, 198.0, 198.0, 0.0, 85.0, 255.0 }));
+    }
 
-	TEST_F(HistogramEqualizationTest, returns_correct_data_size)
-	{
-		auto result = histogramEqualization.scaleData(testIntensities);
-		EXPECT_EQ(result.size(), testIntensities.size());
-	}
+    TEST_F(HistogramEqualizationTest, returns_correct_data_size)
+    {
+        auto result = histogramEqualization.scaleData(testIntensities);
+        EXPECT_EQ(result.size(), testIntensities.size());
+    }
 
-	TEST_F(HistogramEqualizationTest, calculates_new_histogram_data)
-	{
-		std::vector<uint8_t> intensitiesForCalculatingNewDataTest = { 0, 1, 1 };
-		std::vector<unsigned int> cumulativeDistribution(256, 3);
-		cumulativeDistribution[0] = 1;
-		cumulativeDistribution[1] = 3;
-		auto result = histogramEqualization.calculateNewHistogramData(intensitiesForCalculatingNewDataTest, cumulativeDistribution);
-		EXPECT_THAT(result, testing::ContainerEq(std::vector<double>{ 0.0, 255.0, 255.0 }));
+    TEST_F(HistogramEqualizationTest, calculates_new_histogram_data)
+    {
+        std::vector<uint8_t> intensitiesForCalculatingNewDataTest = { 0, 1, 1 };
+        std::vector<unsigned int> cumulativeDistribution(256, 3);
+        cumulativeDistribution[0] = 1;
+        cumulativeDistribution[1] = 3;
+        auto result = histogramEqualization.calculateNewHistogramData(intensitiesForCalculatingNewDataTest, cumulativeDistribution);
+        EXPECT_THAT(result, testing::ContainerEq(std::vector<double>{ 0.0, 255.0, 255.0 }));
     }
 }
