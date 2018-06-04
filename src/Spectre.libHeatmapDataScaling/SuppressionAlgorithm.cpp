9eaf95712a88478c21b732f0e61cf34b03b26184
Daniel Babiak <daniel.babiak8@gmail.com>
2018-06-04 19:39:35 +0200

    Add <cmath> for floor and ceil

--- src/Spectre.libHeatmapDataScaling/SuppressionAlgorithm.cpp
+++ src/Spectre.libHeatmapDataScaling/SuppressionAlgorithm.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <cmath>
 #include "Spectre.libFunctional/Transform.h"
 #include "Spectre.libHeatmapDataScaling/SuppressionAlgorithm.h"
 
