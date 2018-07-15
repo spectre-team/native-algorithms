5bde2f321e3f4bfcb9ea408eb4ff31883e8be7b3
Michal 'Sand3r' Gallus <sand3r@interia.eu>
2018-07-15 14:07:16 +0200

    Add License Headers

--- src/Spectre.libGaussianMixtureModelling/SignalDecomposer.h
+++ src/Spectre.libGaussianMixtureModelling/SignalDecomposer.h
@@ -1,3 +1,23 @@
+/*
+* SignalDecomposer.h
+* Provides implementation of Signal Decomposer algorithm used
+* for Gaussian Mixture Modelling containing functions common
+* for Splitter and Segment decomposing.
+*
+Copyright 2018 Michal Gallus
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #pragma once
 #include "Spectre.libGaussianMixtureModelling/DataTypes.h"
 #include "Spectre.libGaussianMixtureModelling/DynamicProgrammingInitialization.h"
